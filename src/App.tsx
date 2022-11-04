9bc077f40ce08644c3bf4582840dc560d3a58315
Ben Murray <benmurray573@gmail.com>
2022-11-04 12:42:59 +0000

    created PlacesContent which is ready for element inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { PageHeader } from "./utils/PageHeader";
+import PlaceEntry from "./components/Places";
 
 function App(): JSX.Element {
-  return <h1>{PageHeader()}</h1>;
+  return (
+  <div>
+    <h1>{PageHeader()}</h1>
+    <PlaceEntry />
+  </div>
 }
 
 export default App;
---
+++ src/components/PlacesContent.tsx
@@ -0,0 +1,49 @@
+import PlaceEntry from "./Places";
+
+function PlaceContent(): JSX.Element {
+    return (
+        <main>
+            <PlaceEntry
+            title= {}
+            entrynum= {}
+            image= {}
+            description= {}
+            mapslink= {}
+            />
+            <PlaceEntry
+            title= {}
+            entrynum= {}
+            image= {}
+            description= {}
+            mapslink= {}
+            />
+            <PlaceEntry
+            title= {}
+            entrynum= {}
+            image= {}
+            description= {}
+            mapslink= {}
+            />
+            <PlaceEntry
+            title= {}
+            entrynum= {}
+            image= {}
+            description= {}
+            mapslink= {}
+
+
+
+
+
+
+
+
+        </main>
+
+
+
+
+
+    )
+
+}
