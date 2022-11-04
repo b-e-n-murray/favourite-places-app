58f0052e68d57ad4ddf56f2256dc2e30aac4627d
Ben Murray <benmurray573@gmail.com>
2022-11-04 12:12:20 +0000

    created interface and function for PlacesEntry in Places.tsx

--- src/components/Places.tsx
+++ src/components/Places.tsx
@@ -1,13 +1,21 @@
 interface PlacesEntryProps {
     title: string;
     entrynum: number;
-    //image: string;//
+    image: string;
     description: string;
     mapslink: string;   
 }
 
 function PlaceEntry(props: PlacesEntryProps): JSX.Element {
     return (
-        
+        <div>
+            <h2>This webpage will showcase some of my top recommended spots</h2>
+         <h3>Location {props.entrynum}: <a href='#'>{props.title}</a></h3>
+            <img src={props.image} height="600" width="800"></img>
+        <p>{props.description}</p>
+    <p>Find this spot on Google Maps<a href={props.mapslink}>here</a></p>
+        </div>
     )
 }
+
+export default PlaceEntry
