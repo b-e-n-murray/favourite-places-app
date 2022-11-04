e8ce3dc547bbfa1433909e9dfe77b57d87337eb2
Ben Murray <benmurray573@gmail.com>
2022-11-04 12:52:18 +0000

    Transferred first two entries from index to PlaceContent

--- src/components/PlacesContent.tsx
+++ src/components/PlacesContent.tsx
@@ -4,29 +4,29 @@ function PlaceContent(): JSX.Element {
     return (
         <main>
             <PlaceEntry
-            title= {}
-            entrynum= {}
+            entrynum= {1}
+            title= {"Porto Santo"}
+            image= {"https://www.travelsupermarket.com/sonic/image/content/why-visit-porto-santo/porto-santo-beach.jpg"}
+            description= {"A beautiful island off of Portugal with gorgeous beaches."}
+            mapslink= {"https://a.travel-assets.com/findyours-php/viewfinder/images/res70/84000/84406-Okinawa.jpg"}
+            />
+            <PlaceEntry
+            entrynum= {2}
+            title= {"Okinawa"}
+            image= {"https://a.travel-assets.com/findyours-php/viewfinder/images/res70/84000/84406-Okinawa.jpg"}
+            description= {"Description: I've never been, but would love to go and see its rich history and culture. It's also the birthplace of Karate."}
+            mapslink= {"https://www.google.com/maps/place/Okinawa,+Japan/@25.6770863,117.8096098,6z/data=!3m1!4b1!4m5!3m4!1s0x34f57062eeab5be7:0x35bb617286fdd1ef!8m2!3d26.1201911!4d127.7025012?hl=en"}
+            />
+            <PlaceEntry
+            entrynum= {3}
+            title= {"The Gym"}
             image= {}
             description= {}
             mapslink= {}
             />
             <PlaceEntry
-            title= {}
-            entrynum= {}
-            image= {}
-            description= {}
-            mapslink= {}
-            />
-            <PlaceEntry
-            title= {}
-            entrynum= {}
-            image= {}
-            description= {}
-            mapslink= {}
-            />
-            <PlaceEntry
-            title= {}
-            entrynum= {}
+            entrynum= {4}
+            title= {"Bali"}
             image= {}
             description= {}
             mapslink= {}
